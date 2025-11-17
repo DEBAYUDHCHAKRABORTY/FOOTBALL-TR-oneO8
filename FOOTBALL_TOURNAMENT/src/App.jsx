6221d231e5047d193ce59d0a103396ad7cb55e17
DC74 <debayudh.maldaitbss@gmail.com>
2025-11-18 00:42:22 +0530

    Feature/body czar (#4)
    * folders created
    
    * route file added
    
    * body demo done

--- FOOTBALL_TOURNAMENT/src/App.jsx
+++ FOOTBALL_TOURNAMENT/src/App.jsx
@@ -3,10 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/navbar'
 import './App.css'
+import Cards from './components/card'
 
 function App() {
   return (
     <>
+      <Cards/>
     <Navbar/>
     </>
   )
---
+++ FOOTBALL_TOURNAMENT/src/components/card.jsx
@@ -0,0 +1,71 @@
+export default function Cards() {
+  return (
+    <>
+      <div className="ml-20 mt-20">
+        <h1 className="text-4xl font-serif">Tournament Starting...</h1>
+      </div>
+      <div className=" bg-blue-200 mt-10 w-200 font-bold [word-spacing:3px] h-20 justify-center ml-120 rounded-4xl">
+        <h1 className="text-2xl justify-center flex p-5.5">
+          3rd December 2025 onwards at Mathurapur Hospital Field
+        </h1>
+      </div>
+      <div className="ml-20 mt-20">
+        <h1 className="text-4xl font-serif">Sponsered By...</h1>
+      </div>
+      <div className="flex flex-wrap justify-center gap-20 mt-10">
+        <div className="mt-10 rounded-2xl bg-blue-200 w-100 h-80">
+          <h1 className="text-3xl flex justify-center py-35">
+            Choton Jewellery
+          </h1>
+        </div>
+        <div className="mt-10 rounded-2xl bg-blue-200 w-100 h-80">
+          <h1 className="text-3xl flex justify-center py-35">
+            Abhijit Chakraborty
+          </h1>
+        </div>
+        <div className="mt-10 rounded-2xl bg-blue-200 w-100 h-80">
+          <h1 className="text-3xl flex justify-center py-35">
+            Annapurna Jewellery
+          </h1>
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-center gap-20">
+        <div className="mt-10 rounded-2xl bg-blue-200 w-100 h-80">
+          <h1 className="text-3xl flex justify-center py-35">
+            Choton Jewellery
+          </h1>
+        </div>
+        <div className="mt-10 rounded-2xl bg-blue-200 w-100 h-80">
+          <h1 className="text-3xl flex justify-center py-35">
+            Abhijit Chakraborty
+          </h1>
+        </div>
+        <div className="mt-10 rounded-2xl bg-blue-200 w-100 h-80">
+          <h1 className="text-3xl flex justify-center py-35">
+            Annapurna Jewellery
+          </h1>
+        </div>
+      </div>
+      <div className="ml-20 mt-20">
+        <h1 className="text-4xl font-serif">Chief Guests...</h1>
+      </div>
+      <div className="flex flex-wrap justify-center gap-25 mt-10 mb-10">
+        <div className="mt-10 rounded-full bg-blue-200 w-80 h-80">
+          <h1 className="text-3xl flex justify-center py-35">
+            Sabitri Mitra
+          </h1>
+        </div>
+        <div className="mt-10 rounded-full bg-blue-200 w-80 h-80">
+          <h1 className="text-3xl flex justify-center py-35">
+            IC 
+          </h1>
+        </div>
+        <div className="mt-10 rounded-full bg-blue-200 w-80 h-80">
+          <h1 className="text-3xl flex justify-center py-35">
+            DM Madam
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+}
