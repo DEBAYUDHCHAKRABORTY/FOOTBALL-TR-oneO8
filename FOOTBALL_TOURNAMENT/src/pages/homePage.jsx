b54c4c21189f86d916069e21eef9177d4dfcbf3d
Pankaj-web861 <pankajpramanik002@gmail.com>
2025-11-20 12:04:42 +0530

    added few files

--- FOOTBALL_TOURNAMENT/src/pages/homePage.jsx
+++ FOOTBALL_TOURNAMENT/src/pages/homePage.jsx
@@ -3,15 +3,19 @@ import FootballBannar from "../components/bannar";
 import Footer from "../components/footer"
 import Guests from "../components/guests";
 import Navbar from "../components/navbar"
+import Matches from "../components/schedule";
 import Sponsors from "../components/sponsers";
+import Committee from "../components/committee";
 
 export default function HomePage(){
     return(
         <>
             <Navbar/>
             <FootballBannar/>
+            <Matches/>
             <Sponsors/>
             <Guests/>
+            <Committee/>
             <Footer/>
         </>
     );
